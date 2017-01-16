31176dbc4cf4cc2d6dbd7b7db4a596162db31363
ShannonChenCHN <273879688@qq.com>
2017-01-16 14:40:19 +0800

    Add comments to ScrollViewDemo

--- ScrollViewDemo/ScrollViewDemo/FlipAnimationViewController.m
+++ ScrollViewDemo/ScrollViewDemo/FlipAnimationViewController.m
@@ -34,8 +34,11 @@
         self.imageView.transform = CGAffineTransformRotate(self.imageView.transform, M_PI_2);
     }];
     
-    // 水平翻转180°
+    // 水平翻转180°？
     [UIView animateWithDuration:2  delay:2 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+         // MARK: 这里的 CGAffineTransformScale 应该是用来缩放的，要翻转的话推荐用 flip animation，貌似自定义 transform 参数也可以实现，但是比较麻烦
+         // http://stackoverflow.com/questions/9032331/ios-flip-animation-only-for-specific-view
+         // https://segmentfault.com/q/1010000000134502
         self.imageView.transform = CGAffineTransformScale(self.imageView.transform, -1.0, 1.0);
     } completion:NULL];
     
