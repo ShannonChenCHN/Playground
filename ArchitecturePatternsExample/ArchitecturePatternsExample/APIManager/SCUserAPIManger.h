37f2e2b16164f73ae654fdfc68a6f5666869a5fd
ShannonChenCHN <273879688@qq.com>
2017-09-15 00:13:11 +0800

    Update Resume

---
+++ ArchitecturePatternsExample/ArchitecturePatternsExample/APIManager/SCUserAPIManger.h
@@ -0,0 +1,13 @@
+//
+//  SCUserAPIManger.h
+//  ArchitecturePatternsExample
+//
+//  Created by ShannonChen on 2017/9/14.
+//  Copyright © 2017年 ShannonChen. All rights reserved.
+//
+
+#import <YTKNetwork/YTKNetwork.h>
+
+@interface SCUserAPIManger : YTKBaseRequest
+
+@end
