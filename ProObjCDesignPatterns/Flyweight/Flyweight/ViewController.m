368a81996d101429ead719a30e14d57a811fdedd
ShannonChenCHN <273879688@qq.com>
2017-11-20 19:37:18 +0800

    Update ProObjCDesignPatterns

--- ProObjCDesignPatterns/Flyweight/Flyweight/ViewController.m
+++ ProObjCDesignPatterns/Flyweight/Flyweight/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "FlyweightView.h"
+#import "FlowerFactory.h"
 
 @interface ViewController ()
 
@@ -14,15 +16,42 @@
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    FlowerFactory *factory = [[FlowerFactory alloc] init];
+    NSMutableArray *flowerList = [[NSMutableArray alloc] initWithCapacity:500];
+    
+    for (int i = 0; i < 500; ++i) {
+
+        // 从 享元工厂 中获取对应的 享元产品
+        FlowerType flowerType = arc4random_uniform(kTotalNumberOfFlowerTypes) % kTotalNumberOfFlowerTypes;
+        UIView *flowerView = [factory flowerViewWithType:flowerType];
+        
+        // 计算位置尺寸信息
+        CGRect screenBounds = [[UIScreen mainScreen] bounds];
+        CGFloat x = (arc4random() % (NSInteger)screenBounds.size.width);
+        CGFloat y = (arc4random() % (NSInteger)screenBounds.size.height);
+        NSInteger minSize = 10;
+        NSInteger maxSize = 50;
+        CGFloat size = (arc4random() % (maxSize - minSize + 1)) + minSize;
+        
+        
+        // 封装信息
+        ExtrinsicFlowerState *extrinsicState = [[ExtrinsicFlowerState alloc] init];
+        extrinsicState.flowerView = flowerView;
+        extrinsicState.area = CGRectMake(x, y, size, size);
+        
+        [flowerList addObject:extrinsicState];
+    }
+    
+    [(FlyweightView *)self.view setFlowerList:flowerList];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)loadView {
+    self.view = [[FlyweightView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.view.backgroundColor = [UIColor whiteColor];
 }
 
 
