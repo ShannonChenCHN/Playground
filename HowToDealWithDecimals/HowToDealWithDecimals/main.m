681b69fc06c4ea964f283890f6df77c7e05d7cd6
ShannonChen <shannonchenchn@foxmail.com>
2018-07-05 10:33:47 +0800

    Update HowToDealWithDecimals

--- HowToDealWithDecimals/HowToDealWithDecimals/main.m
+++ HowToDealWithDecimals/HowToDealWithDecimals/main.m
@@ -8,6 +8,32 @@
 
 #import <Foundation/Foundation.h>
 
+void printBinary(float num) {
+    unsigned long buff;
+    char s[35];
+    
+    // 将数据复制到 4 字节长度的整数变量 buff 中以逐个提出出每一位
+    memcpy(&buff, &num, 4);
+    
+    // 逐一取出每一位
+    for (int i = 33; i >= 0; i--) {
+        if (i == 1 || i == 10) {
+            s[i] = '-';
+        } else {
+            if (buff % 2 == 1) {
+                s[i] = '1';
+            } else {
+                s[i] = '0';
+            }
+            buff /= 2;
+        }
+    }
+    
+    s[34] = '\0';
+    
+    printf("%s\n", s);
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         float a = 102100.00 * 2;
